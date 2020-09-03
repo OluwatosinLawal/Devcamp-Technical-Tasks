3747bd5235fa51f772bdf3e672e9dda5f8a1bffa
OluwatosinLawal <67714411+OluwatosinLawal@users.noreply.github.com>
2020-09-03 20:41:39 +0100

    Add files via upload

---
+++ Task_8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Task 8")
+print("")
+Given_nos_in_str ="0, 1, 2, 3, 3, 3, 3, 5, 3,5,6,3,3,32!!!"
+nos_of_3s = '3'
+print ("nos_of_3s : ", Given_nos_in_str.count(nos_of_3s))
+
