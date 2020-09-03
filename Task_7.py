c491300975016c4b0def09b288d1488754367bd2
OluwatosinLawal <67714411+OluwatosinLawal@users.noreply.github.com>
2020-09-03 20:17:39 +0100

    Add files via upload

---
+++ Task_7.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#Here we will get the standard deviation of a sample
+
+import statistics     #To import the statistics module
+X = [3,2,5,4,7]       #The sample will be called X with some given numbers
+
+print ("Standard Deviation of X is % s " % (statistics.stdev(X)))
+
